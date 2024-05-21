b64ee30ac14f5a8cd8300d3985f258d43929719f
whereugo <elbar.ace@mail.ru>
2024-05-22 02:43:10 +0700

    asd

--- src/features/surveys/surveysApiSlice.js
+++ src/features/surveys/surveysApiSlice.js
@@ -7,29 +7,39 @@ import { apiSlice } from "../api/apiSlice";
 const surveysApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getSurveys: builder.query({
-      query: () => "/main/survey/", 
+      query: () => "/main/survey/",
       providesTags: result => ['Surveys']
     }),
     getSurveySingle: builder.query({
-      query: (id) => `/main/survey/${id}`, 
+      query: (id) => `/main/survey/${id}`,
       providesTags: result => ['SurveySingle']
     }),
     getSurveyIdQuestions: builder.query({
-      query: (id) => `/main/survey/${id}/questions`, 
+      query: (id) => `/main/survey/${id}/questions`,
       providesTags: result => ['SurveySingleQuestions']
     }),
     getSurveyIdOption: builder.query({
-      query: (questionId) => `/main/question/${questionId}/options/`, 
+      query: (questionId) => `/main/question/${questionId}/options/`,
       providesTags: result => ['Options']
     }),
+
     createSurveyIdQuestions: builder.mutation({
-      query: (id) => ({
-        url: `/main/survey/${id}/questions`,
+      query: (questionData) => ({
+        url: `/main/survey/${questionData.surveyId}/questions`,
         method: 'POST',
-        body: id
+        body: questionData,
       }),
       invalidatesTags: ['SurveySingleQuestions']
     }),
+    createSurveyIdOptions: builder.mutation({
+      query: (optionData) => ({
+        url: `/main/question/${optionData.questionId}/options/`,
+        method: 'POST',
+        body: optionData,
+      }),
+      invalidatesTags: ['SurveySingleQuestions']
+    }),
+
     createSurveys: builder.mutation({
       query: (body) => ({
         url: '/main/survey/',
@@ -38,6 +48,7 @@ const surveysApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ['Surveys']
     }),
+
     updateSurveys: builder.mutation({
       query: (survey) => ({
         url: `/main/survey/${survey.id}/`,
@@ -46,7 +57,15 @@ const surveysApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ['Surveys']
     }),
-    
+    updateSurveyIdOptions: builder.mutation({
+      query: (questionId) => ({
+        url: `/main/question/${questionId}/options/`,
+        method: 'POST',
+        body: questionId
+      }),
+      invalidatesTags: ['SurveySingleQuestions']
+    }),
+
     deleteSurveys: builder.mutation({
       query: (survey) => ({
         url: `/main/survey/${survey.id}/`,
@@ -54,20 +73,25 @@ const surveysApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ['Surveys']
     }),
+    deleteQuestion: builder.mutation({
+      query: (id) => ({
+        url: `/main/question/${id}/`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['SurveySingleQuestions']
+    }),
   })
-})
+});
 
-
-export const { 
+export const {
   useCreateSurveysMutation,
   useGetSurveysQuery,
   useDeleteSurveysMutation,
   useUpdateSurveysMutation,
-
   useGetSurveySingleQuery,
   useGetSurveyIdQuestionsQuery,
   useCreateSurveyIdQuestionsMutation,
-
+  useDeleteQuestionMutation,
   useGetSurveyIdOptionQuery,
-
+  useCreateSurveyIdOptionsMutation,
 } = surveysApiSlice;
--- src/pages/Surveys/Survey/Survey.jsx
+++ src/pages/Surveys/Survey/Survey.jsx
@@ -2,27 +2,32 @@ import React, { useEffect, useState } from "react";
 import styles from "./Survey.module.scss";
 import Header from "../../../components/Header/Header";
 import { useNavigate, useParams } from "react-router-dom";
-import { useCreateSurveyIdQuestionsMutation, useDeleteSurveysMutation, useGetSurveyIdOptionQuery, useGetSurveyIdQuestionsQuery, useGetSurveySingleQuery } from "../../../features/surveys/surveysApiSlice";
+import { useCreateSurveyIdOptionsMutation, useCreateSurveyIdQuestionsMutation, useDeleteQuestionMutation, useDeleteSurveysMutation, useGetSurveyIdQuestionsQuery, useGetSurveySingleQuery } from "../../../features/surveys/surveysApiSlice";
 import { useDispatch } from "react-redux";
 import { apiSlice } from "../../../features/api/apiSlice";
 import MyInput from "../../../components/MyInput/MyInput";
 import { ROUTES } from "../../../utils/routes";
+import Modal from "../../../components/Modal/Modal";
+
 
 const Survey = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
+  const [modalActive, setModalActive] = useState(false);
+  const [questionText, setQuestionText] = useState('');
+  const [questionType, setQuestionType] = useState('');
+  const [options, setOptions] = useState([]);
   const { data: survey, isLoading, error } = useGetSurveySingleQuery(id);
   const { data: questions, isLoading: isLoadingQuestions, error: errorQuestions } = useGetSurveyIdQuestionsQuery(id);
-  
-  const [createQuestion, { isLoading: isCreating }] = useCreateSurveyIdQuestionsMutation();
-  const [deleteSurvey, {}] = useDeleteSurveysMutation()
+  const [createSurveyIdOptions, {}] = useCreateSurveyIdOptionsMutation()
 
+  const [createQuestion, { isLoading: isCreating }] = useCreateSurveyIdQuestionsMutation();
+  const [deleteSurvey] = useDeleteSurveysMutation();
+  const [deleteQuestion] = useDeleteQuestionMutation();
 
   const [selectedStatus, setSelectedStatus] = useState('-');
   const [selectedTypes, setSelectedTypes] = useState({}); 
-  const [options, setOptions] = useState({});
 
   useEffect(() => {
     if (survey) {
@@ -68,20 +73,74 @@ const Survey = () => {
   if (error) return <div>Error loading survey: {error.message}</div>;
   if (errorQuestions) return <div>Error loading questions: {errorQuestions.message}</div>;
 
-  const handleAddQuestion = async () => {
-    try {
-      await createQuestion({ id: id, question_type: selectedTypes, text: questions.text });
-    } catch (e) {
-      console.log("Не удалось создать вопрос", e);
-    }
+  const handleOpenModal = () => {
+    setModalActive(true);
   };
+
+  const handleAddQuestion = async (e) => {
+    e.preventDefault();
+    try {
+        const questionData = {
+            question_type: questionType,
+            text: questionText,
+        };
+
+        // Создаем вопрос
+        const questionResponse = await createQuestion(questionData).unwrap();
+        const questionId = questionResponse.id;
+
+        // Если вопрос типа MULTIPLE_CHOICE, создаем опции
+        if (questionType === 'MULTIPLE_CHOICE' && options.length > 0) {
+            const optionPromises = options.map(option => {
+                const optionData = { questionId, text: option };
+                return createSurveyIdOptions(optionData);
+            });
+            await Promise.all(optionPromises);
+        }
+
+        // Сброс формы и закрытие модального окна
+        setModalActive(false);
+        setQuestionText('');
+        setQuestionType('');
+        setOptions([]);
+    } catch (e) {
+        console.log("Не удалось создать вопрос", e);
+    }
+};
 
   const handleRemove = (surveyId) => {
     const result = window.confirm("Вы точно хотите удалить сообщение?");
     if (result) {
       deleteSurvey({ id: surveyId });
-      navigate(ROUTES.SURVEYS)
+      navigate(ROUTES.SURVEYS);
     }
+  };
+
+  const handleRemoveQuestion = async (questionId) => {
+    const result = window.confirm("Вы точно хотите удалить вопрос?");
+    if (result) {
+      try {
+        await deleteQuestion(questionId);
+      } catch (e) {
+        console.log("Не удалось удалить вопрос", e);
+      }
+    }
+  };
+
+  const handleAddOption = () => {
+    setOptions([...options, '']);
+  };
+
+  const handleOptionChange = (index, value) => {
+    const newOptions = [...options];
+    newOptions[index] = value;
+    setOptions(newOptions);
+  };
+
+  const handleRemoveOption = (index) => {
+    const newOptions = [...options];
+    newOptions.splice(index, 1);
+    setOptions(newOptions);
   };
 
   return (
@@ -91,7 +150,6 @@ const Survey = () => {
         <div className={styles.content}>
           {survey && (
             <div className={styles.survey__wrapp} key={survey.id}>
-
               <div className={`${styles.input_wrapp} ${styles.input_wrapp_title}`}>
                 <div className={styles.title_input}>
                   <label htmlFor="title">Название:</label>
@@ -131,7 +189,7 @@ const Survey = () => {
           <div className={styles.title_question}>
             <h3>Вопросы:</h3>
             <MyInput />
-            <button onClick={handleAddQuestion}>Добавить</button>
+            <button onClick={handleOpenModal}>Добавить</button>
           </div>
           {questions && questions.map((question) => (
             <div key={question.id} className={styles.question}>
@@ -156,17 +214,65 @@ const Survey = () => {
                 <textarea id={`question-${question.id}`} rows={3} defaultValue={question.text}></textarea>
                 {question.question_type === 'MULTIPLE_CHOICE' && (
                   <>
-                    <label htmlFor={`options-${question.id}`}>Варианты ответов:</label>
+                    <label htmlFor={`options-${question.id}`}>Варианты ответов: <button>Добавить</button></label>
                     {options[question.id] && options[question.id].map(option => (
-                      <textarea key={option.id} id={`option-${option.id}`}>{option.text}</textarea>
+                      <textarea key={option.id} defaultValue={option.text}></textarea>
                     ))}
                   </>
                 )}
+                <div className={styles.buttons}>
+                  <button onClick={() => handleRemoveQuestion(question.id)}>Удалить</button>
+                  <button>Сохранить</button>
+                </div>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <form className={styles.form} onSubmit={handleAddQuestion}>
+          <div className={styles.select_wrapp}>
+            <label htmlFor="types-select">Выберите тип:</label>
+            <select
+              required
+              name="types"
+              id="types-select"
+              className={styles.select}
+              value={questionType}
+              onChange={(e) => setQuestionType(e.target.value)}
+            >
+              <option value="">-----</option>
+              <option value="MULTIPLE_CHOICE">Множественный выбор</option>
+              <option value="OPEN_ENDED">Открытый ответ</option>
+            </select>
+          </div>
+          <textarea
+            required
+            rows={10}
+            className={styles.input}
+            value={questionText}
+            onChange={(e) => setQuestionText(e.target.value)}
+          />
+          {questionType === 'MULTIPLE_CHOICE' && (
+            <div className={styles.options}>
+              <label>Опции:</label>
+              {options.map((option, index) => (
+                <div key={index} className={styles.option}>
+                  <textarea
+                    value={option}
+                    onChange={(e) => handleOptionChange(index, e.target.value)}
+                  />
+                  <button type="button" onClick={() => handleRemoveOption(index)}>Удалить</button>
+                </div>
+              ))}
+              <button type="button" onClick={handleAddOption}>Добавить опцию</button>
+            </div>
+          )}
+          <button className={styles.saveBtn}>
+            Создать
+          </button>
+        </form>
+      </Modal>
     </>
   );
 };
